f260b8a3420332628921287836dcaa0f48010fbd
Matheus <matheusgarciadg@gmail.com>
2023-11-11 16:58:44 -0300

    test: custom commands and selectors

--- testes-e2e-cypress-main/cypress/e2e/start.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/start.cy.js
@@ -1,5 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Home page', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000')
+  })
+  it('should renders h1 with correct text', () => {
+    cy.getByData('titulo-principal').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
+  })
+  it('should render vantagens text', () => {
+    cy.getTextBySelector('h2', 'Vantagens do nosso banco:')
   })
 })

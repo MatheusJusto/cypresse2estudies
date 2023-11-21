a58ca52e78a47a4401cd8b030c26e6a935443d2a
Matheus <matheusgarciadg@gmail.com>
2023-11-20 23:50:05 -0300

    test: new commands and session storage config

--- testes-e2e-cypress-v2-main/cypress/e2e/formularioLogin.cy.js
+++ testes-e2e-cypress-v2-main/cypress/e2e/formularioLogin.cy.js
@@ -1,20 +1,24 @@
 describe('Formulario de Login', ()=>{
-  beforeEach(()=>{
-    cy.visit('http://localhost:3000')
+
+
+  it('Should acces home page', () => {
+    cy.login('matheus@matheus.com', '123')
+    cy.visit('/home')
+    cy.getByData('titulo-boas-vindas').should('contain', 'Bem vindo de volta!')
   })
 
-  it('Não deve permitir um email inválido', ()=>{
-    cy.getByData('botao-login').click()
-    cy.getByData('email-input').type('neilton@alura')
-    cy.getByData('senha-input').type('123456')
-    cy.getByData('botao-enviar').click()
-    cy.getByData('mensagem-erro').should('exist').and('have.text', 'O email digitado é inválido')
-  })
+  // it('Não deve permitir um email inválido', ()=>{
+  //   cy.getByData('botao-login').click()
+  //   cy.getByData('email-input').type('neilton@alura')
+  //   cy.getByData('senha-input').type('123456')
+  //   cy.getByData('botao-enviar').click()
+  //   cy.getByData('mensagem-erro').should('exist').and('have.text', 'O email digitado é inválido')
+  // })
 
-  it.only('Não deve permitir um campo em branco', ()=>{
-    cy.getByData('botao-login').click()
-    cy.getByData('senha-input').type('123456')
-    cy.getByData('botao-enviar').click()
-    cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo email é obrigatório')
-  })
+  // it.only('Não deve permitir um campo em branco', ()=>{
+  //   cy.getByData('botao-login').click()
+  //   cy.getByData('senha-input').type('123456')
+  //   cy.getByData('botao-enviar').click()
+  //   cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo email é obrigatório')
+  // })
 })
